f5b8720cbec2a769b876c3d785669ae3932c4d27
Gyanendra <gyanen56@gmail.com>
2024-06-17 13:26:07 +1000

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import profile from "./assets/images/gyan.png"
+import project from "./assets/images/project.png"
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+   hello
+    </>
+  )
+}
+
+export default App
