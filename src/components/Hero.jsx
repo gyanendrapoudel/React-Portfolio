2f721cdbfd3930300839a8292ccec6ffad311aaf
Gyanendra <gyanen56@gmail.com>
2024-06-20 09:25:13 +1000

    only showing go up icon when scrolling down

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,11 @@ import profile from '../assets/images/gyan.png'
 const Hero = () => {
   return (
     <>
-    <section class="about-me pad-maker" id="about-me">
-      <p class="name-text">
+    <section className="about-me pad-maker" id="about-me">
+      <p className="name-text">
         <span style={{ fontSize: '12px' }}>Hi I am </span>Gyanendra Poudel
       </p>
-      <div class="">Soft. Engineer</div>
+      <div className="">Soft. Engineer</div>
       <p>
         I love coding. <br />I am Passionate coder proficient in multiple
         languages, <br />
@@ -14,29 +14,29 @@ const Hero = () => {
         turning ideas into functional software solutions.{' '}
       </p>
       <a href={profile} download>
-        <button class="btn">
-          Download CV <i class="fa-solid fa-download"></i>
+        <button className="btn">
+          Download CV <i className="fa-solid fa-download"></i>
         </button>
       </a>
-      <div class="about-me-img">
+      <div className="about-me-img">
         <img src={profile} alt="" />
       </div>
     </section>
-     <section class="pad-maker experiece display-flex">
-            <div class="display-flex">
-                <div class="icon" >
-                  <a href=""> <i class="fa-solid fa-graduation-cap"></i></a>
+     <section className="pad-maker experiece display-flex">
+            <div className="display-flex">
+                <div className="icon" >
+                  <a href=""> <i className="fa-solid fa-graduation-cap"></i></a>
                 </div>
                 <div >
                     <h4>Master in Software Engineer</h4>
                     <p>Graduation</p>
                 </div>
             </div>
-            <div class="line-separator"></div>
+            <div className="line-separator"></div>
 
-            <div class="display-flex">
-                <div class="icon">
-                    <a href=""> <i class="fa-solid fa-book-open"></i></a>
+            <div className="display-flex">
+                <div className="icon">
+                    <a href=""> <i className="fa-solid fa-book-open"></i></a>
 
                 </div>
                 <div>
@@ -44,11 +44,11 @@ const Hero = () => {
                     <p>Projects</p>
                 </div>
             </div>
-            <div class="line-separator"></div>
+            <div className="line-separator"></div>
 
-            <div class="display-flex">
-                <div class="icon">
-                    <a href=""><i class="fa-solid fa-terminal"></i></a>
+            <div className="display-flex">
+                <div className="icon">
+                    <a href=""><i className="fa-solid fa-terminal"></i></a>
                 </div>
                 <div>
                     <h4>1+ year Experience</h4>
