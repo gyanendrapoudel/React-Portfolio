3e26276042ed0e571d6f31b74cd59850ab806dca
Gyanendra <gyanen56@gmail.com>
2024-06-19 18:12:07 +1000

    using map for projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,42 +1,43 @@
 import project from '../assets/images/project.png'
 import Project from './Project'
 import Title from './Title'
+const details = [
+  {
+    webLink: 'https://github.com/gyanendrapoudel/jobster',
+    gitLink: 'https://redux-toolkit-jobster.netlify.app/landing',
+    projectTitle: 'Jobster Development',
+    techStack: 'TechStack: React, Redux, thunkAP',
+    name: 'project1',
+  },
+  {
+    webLink: 'https://gyanendrapoudel.netlify.app/',
+    gitLink: 'https://github.com/gyanendrapoudel/tours',
+    projectTitle: 'Tour Blog',
+    techStack: 'TechStack: React, CSS',
+    name: 'project2',
+  },
+  { webLink:'https://gyanendrapoudel.github.io/YouTubeClone/',
+    gitLink:'https://github.com/gyanendrapoudel/YouTubeClone',
+    projectTitle:'YouTube Clone',
+    techStack:'TechStack: HTML, CSS',
+    name:'project3'
+  },
+  {
+     webLink:'https://github.com/gyanendrapoudel/Parallex',
+          gitLink:'https://gyanendrapoudel.github.io/Parallex/',
+          projectTitle:'Parallax Effect Project',
+          techStack:'TechStack: HTML, CSS',
+          name:'project4',
+  }
+] 
 const Projects = () => {
   return (
     <section className="hero pad-maker" id="project">
       <h4>my Project</h4>
       <div className="projects">
-        <Project
-          webLink={'https://github.com/gyanendrapoudel/jobster'}
-          gitLink={'https://redux-toolkit-jobster.netlify.app/landing'}
-          projectTitle={'Jobster Development'}
-          techStack={'TechStack: React, Redux, thunkAP'}
-          name={'project1'}
-        />
-
-        <Project
-          webLink={'https://gyanendrapoudel.netlify.app/'}
-          gitLink={'https://github.com/gyanendrapoudel/tours'}
-          projectTitle={'Tour Blog'}
-          techStack={'TechStack: React, CSS'}
-          name={'project2'}
-        />
-
-        <Project
-          webLink={'https://gyanendrapoudel.github.io/YouTubeClone/'}
-          gitLink={'https://github.com/gyanendrapoudel/YouTubeClone'}
-          projectTitle={'YouTube Clone'}
-          techStack={'TechStack: HTML, CSS'}
-          name={'project3'}
-        />
-        <Project
-          webLink={'https://github.com/gyanendrapoudel/Parallex'}
-          gitLink={'https://gyanendrapoudel.github.io/Parallex/'}
-          projectTitle={'Parallax Effect Project'}
-          techStack={'TechStack: HTML, CSS'}
-          name={'project4'}
-        />
-
+        {details.map((detail,index)=>{
+            return <Project key={index} {...detail}/>
+        })}
         
       </div>
     </section>
